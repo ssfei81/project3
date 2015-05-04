efd030933e685aa413417a2f5ec85d7cb9e6ce8d
Sun Shuo <ssfei81@hotmail.com>
2015-05-04 04:17:20 -0700

    random delay to D2 working

--- destination.cpp
+++ destination.cpp
@@ -4,7 +4,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
 #include "packet.h"
 using namespace std;
 
@@ -33,6 +35,10 @@ int main(int argc, char *argv[])
     struct sockaddr_in svrAddr, cliAddr;
     char *buf;
 
+    //additional variables for project 3
+    float b = 5;
+    double timer = 0;
+
     cout<<"************************************"<<endl;
     cout<<"*** Welcome to EE 122 Project #2 ***"<<endl;
     cout<<"***     Shuo Sun and Gabriel     ***"<<endl;
@@ -41,7 +47,7 @@ int main(int argc, char *argv[])
     cout<<"(ctrl + c to exit)"<<endl;
 
     //read from command line
-    if (argc != 2) errorMsg("Invalid input format"); 
+    if (argc != 2 && argc != 3) errorMsg("Invalid input format"); 
 
     portno = atoi(argv[1]);
     if(portno > 65535 || portno < 0) error("Port number must be between 0 - 65535");
@@ -63,14 +69,31 @@ int main(int argc, char *argv[])
     cout<<"Done"<<endl;
 
     cout<<"Initializing receiver...Done"<<endl;
-    cout<<"Initializing buffer...";
+    cout<<"Initializing buffer..."<<endl;
     buf = new char[MAX_PACKET_SIZE];
     int count = 1;
 
     struct timeval tv;
     int delaySum = 0;
-    while(1) 
+    //----------------------
+    //project 3 delay
+    struct timeval start;
+    struct timeval end;
+    //----------------------
+
+   while(1) 
     {
+        /////////////////////////
+        // project 3
+        gettimeofday(&start,NULL);
+        int randomDelay = rand()%(int)(b * 1000);
+        float randomDelay2 = (float)randomDelay/1000;
+        if(argc==3 && strcmp(argv[2], "-D")==0){
+        //if destination is D2 then add random delay
+        usleep(randomDelay2);
+        }
+        //////////////////////////
+        
         cout<<"Receiving Packet "<< count <<endl;
         int n = recvfrom(sockfd, buf, MAX_PACKET_SIZE, 0, (struct sockaddr *)&cliAddr, &clilen);
         count += 1;
@@ -88,7 +111,26 @@ int main(int argc, char *argv[])
         unsigned long long delay = msSinceEpoch - msSinceEpoch2;
         //cout<<delay<<endl;
         delaySum += delay;
-        cout<<delaySum<<endl;
+        //cout<<delaySum<<endl;
+
+    //-------------------------
+    //project 3 delay
+    gettimeofday(&end, NULL);
+    unsigned long long s_time = (unsigned long long)(start.tv_sec)*1000 + (unsigned long long)(start.tv_usec)/1000;
+
+unsigned long long e_time = (unsigned long long)(end.tv_sec)*1000 + (unsigned long long)(end.tv_usec)/1000;
+
+    double duration = (double) (e_time - s_time) / 1000;
+    timer += duration;
+
+    if(timer > 5) {
+        if(b == 5) b = 15;
+        else b = 5;
+        timer = 0;
+    }
+    cout<<timer<<"("<<b<<")"<<endl;
+    //------------------------
+
     }
     return 0;
 }
